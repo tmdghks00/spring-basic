4d3a1e0d9f972ce2e5bc3796a00d007427c9fd23
SeungHwan Oh <wxtq22@naver.com>
2025-03-16 14:48:44 +0900

    .

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -2,37 +2,37 @@ package hello.core.member;
 
 public class Member {
 
-    private Long id;
-    private String name;
-    private Grade grade;
+    private Long id; // 회원 ID
+    private String name; // 회원 이름
+    private Grade grade; // 회원 등급
 
-    public Member(Long id, String name, Grade grade) {
+    public Member(Long id, String name, Grade grade) { // 생성자
         this.id = id;
         this.name = name;
         this.grade = grade;
     }
 
-    public Long getId() {
+    public Long getId() { // 회원 ID 반환
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id) { // 회원 ID 설정
         this.id = id;
     }
 
-    public String getName() {
+    public String getName() { // 회원 이름 반환
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) { // 회원 이름 설정
         this.name = name;
     }
 
-    public Grade getGrade() {
+    public Grade getGrade() { // 회원 등급 반환
         return grade;
     }
 
-    public void setGrade(Grade grade) {
+    public void setGrade(Grade grade) { // 회원 등급 설정
         this.grade = grade;
     }
 }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,20 +5,18 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-
-@Component
-
+@Component // 스프링 빈으로 등록
 public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> store = new HashMap<>(); // 회원 정보를 저장하는 HashMap
 
     @Override
-    public void save(Member member) {
+    public void save(Member member) { // 회원 정보 저장
         store.put(member.getId(), member);
     }
 
     @Override
-    public Member findById(Long memberId) {
+    public Member findById(Long memberId) { // 회원 ID로 회원 정보 조회
         return store.get(memberId);
     }
 }
