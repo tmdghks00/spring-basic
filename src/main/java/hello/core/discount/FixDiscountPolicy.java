0d73b0e99d7253046df2bf7a85035ed9e03801f3
SeungHwan Oh <wxtq22@naver.com>
2025-03-17 19:48:37 +0900

    .

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,12 +5,12 @@ import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component // 해당 클래스를 스프링 컨테이너에 빈(Bean)으로 등록
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; //1000원 할인
 
-    @Override
+    @Override // 부모 클래스(또는 인터페이스)의 메서드를 재정의
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) { //  등급이 VIP
             return discountFixAmount; // VIP면 1000원 할인
